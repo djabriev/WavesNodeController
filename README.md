4c80d2267a583b44e97d82fa2dec2c1081b91861
entrypoint <admin@test.com>
2022-06-10 00:24:42 +0500

    Hello there xD

---
+++ README.md
@@ -0,0 +1,61 @@
+Control panel for waves node (ubuntu only i guess), implemented through telegram bot api (bot will react only to your chat_id, so no one besides you can interact with bot)
+
+Yeah shitcode, but i'm trying be helpful xD
+
+## Functionality
+![alt text](https://i.ibb.co/mhwMS7M/image.png)
+
+## How it works
+
+- Works on waves service (docker not supported), tested on ubuntu only
+- Node update through deb package
+- Node height/version/features through local rest api
+- Restart/update through ```os.system```
+- Enable next feature - script open waves.conf file and looks for string ```supported = [``` then replaces it with ```supported = [id,``` (hardcoded like this to avoid external package imports)
+- [Can be turned off by ```DISTRIBUTE_REWARDS = False```] Distribute rewards just runs external script that you should provide (example: https://github.com/waves-exchange/neutrino-utilities/blob/main/neutrino_nodes/payment.py)
+
+## Requirements
+- Correctly installed node (https://docs.waves.tech/en/waves-node/how-to-install-a-node/on-ubuntu)
+- Ubuntu
+- Python 3
+- Your node .conf file should contain features settings (waves.features)
+```
+waves {
+       ...
+       features {
+           supported = [17]
+       }
+       ...
+} 
+```
+- Telegram bot (bot token and your user_id, user_id can be found using @chat_id_echo_bot)
+- If you are going to use distribute rewards functionality, then prepare python script for this job and provide path to this python file in DISTRIBUTE_REWARDS_SCRIPT variable, script will be executed through ```exec``` function
+- Fill variable values at the top of file, required to change: ```TOKEN```, ```ADMIN_CHAT_ID```, ```DISTRIBUTE_REWARDS_SCRIPT``` (if you are going to use rewards distribution), the rest of the constants as you wish
+
+## Tips
+I'm using this script as service to keep it alive and run automatically after restart of server
+
+Steps to reproduce:
+- Create file /lib/systemd/system/test-py.service
+- Check your python executable path to be correct ExecStart=/usr/bin/python3.10
+- Right after the executable path change path to this script, by default it's: /home/main.py
+```
+[Unit]
+Description=Test Service
+Requires=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/python3.10 /home/main.py
+Restart=always
+RestartSec=60
+User=root
+PermissionsStartOnly=true
+TimeoutStopSec=90
+
+[Install]
+WantedBy=multi-user.target
+```
+- ```sudo systemctl daemon-reload```
+- ```sudo systemctl enable test-py.service``` (or how you called it while creating service file)
+- ```sudo systemctl start test-py.service```
---
+++ main.py
@@ -0,0 +1,197 @@
+# made by @entrypoint
+# works on waves service (docker not supported), tested on ubuntu only
+# node update through deb package
+# your node .conf file should contain features settings (waves.features)
+#   waves {
+#       ...
+#       features {
+#           supported = [17]
+#       }
+#       ...
+#   }
+# script looks for string 'supported = [' then replaces it with 'supported = [id,' // hardcoded like this to avoid
+#                                                                                     external package imports
+
+import json
+import os
+import threading
+
+import requests
+import time
+
+TOKEN = 'telegram_bot_tokem'
+URL = 'https://api.telegram.org/bot'
+ADMIN_CHAT_ID = 0  # get your chat id here: @chat_id_echo_bot
+MY_NODE_ADDRESS = 'http://localhost:6870'
+WAVES_NODE_ADDRESS = 'https://nodes.wavesnodes.com'
+DISTRIBUTE_REWARDS = False  # Distribute rewards functionality
+DISTRIBUTE_REWARDS_AUTO = False  # Distributing rewards automatically in n seconds
+DISTRIBUTE_REWARDS_SCRIPT = '/home/distribute.py'  # path to your distribute rewards python script
+DISTRIBUTE_REWARDS_INTERVAL = 21600  # auto rewards distribution interval (seconds), only if DISTRIBUTE_REWARDS_AUTO = True
+NODE_CONFIG_DIRECTORY = '/usr/share/waves/conf'
+
+
+def get_updates(offset=0):
+    result = requests.get(f'{URL}{TOKEN}/getUpdates?offset={offset}').json()
+    return result['result']
+
+
+def send_text(bot_message):
+    url = f'{URL}{TOKEN}/sendMessage?chat_id={ADMIN_CHAT_ID}&text={bot_message}'
+    requests.get(url)
+
+
+def start():
+    kb = json.dumps(
+        {"keyboard":
+            [
+                [
+                    {"text": "check node height"},
+                    {"text": "check node version"},
+                ],
+                [
+                    {"text": "check node features"},
+                    {"text": "restart node"},
+
+                ],
+                [
+                    {"text": "distribute rewards"},
+                    {"text": "update node"},
+                ],
+                [
+                    {"text": "enable next feature"},
+                ]
+            ]
+        }
+    )
+
+    # Create data dict
+    data = {
+        'text': (None, 'Hi!'),
+        'chat_id': (None, ADMIN_CHAT_ID),
+        'reply_markup': (None, kb)
+    }
+
+    requests.post(url=f'{URL}{TOKEN}/sendMessage', headers={}, files=data)
+
+
+def check_node_height():
+    waves_node_result = requests.get(f'{WAVES_NODE_ADDRESS}/blocks/height').json()['height']
+    my_node_result = requests.get(f"{MY_NODE_ADDRESS}/blocks/height").json()['height']
+    return f"Waves node height: {waves_node_result}\nMy node height: {my_node_result}"
+
+
+def check_node_version():
+    return requests.get(f'{MY_NODE_ADDRESS}/node/version').json()['version']
+
+
+def check_node_features():
+    features = requests.get(f'{MY_NODE_ADDRESS}/activation/status').json()['features']
+    return json.dumps(features, indent=2)
+
+
+def distribute_rewards():
+    if DISTRIBUTE_REWARDS:
+        exec(open(DISTRIBUTE_REWARDS_SCRIPT).read())
+        send_text('rewards distributed')
+    else:
+        send_text('functionality disabled')
+
+
+def restart_node():
+    os.system('systemctl restart waves.service')
+    send_text('node restarted')
+
+
+def enable_next_feature():
+    node_features = json.loads(check_node_features())
+
+    print(node_features[-1]['nodeStatus'])
+
+    if node_features[-1]['nodeStatus'] == 'VOTED':
+        send_text('Already enabled')
+    else:
+        with open(f'{NODE_CONFIG_DIRECTORY}/waves.conf') as file:
+            file_str = file.read()
+            feature_id = node_features[-1]['id']
+            file_str = file_str.replace('supported = [', f'supported = [{feature_id},')
+
+            with open(f'{NODE_CONFIG_DIRECTORY}/waves.conf', 'w') as outfile:
+                outfile.write(file_str)
+
+            send_text(f'Feature {feature_id} successfully enabled')
+
+            restart_node()
+
+
+def run_telegram_bot():
+    update_id = get_updates()[-1]['update_id']
+    while True:
+        time.sleep(3)
+
+        try:
+            messages = get_updates(update_id)
+            for message in messages:
+                if update_id < message['update_id']:
+                    update_id = message['update_id']
+                    message_chat_id = message['message']['chat']['id']
+                    message = message['message']['text']
+
+                    if ADMIN_CHAT_ID == message_chat_id:
+                        print(f"Message: {message}")
+
+                        if message == '/start':
+                            start()
+
+                        elif message == 'check node height':
+                            send_text(check_node_height())
+
+                        elif message == 'check node version':
+                            send_text(check_node_version())
+
+                        elif message == 'check node features':
+                            send_text(check_node_features())
+
+                        elif message == 'restart node':
+                            restart_node()
+
+                        elif message == 'distribute rewards' and DISTRIBUTE_REWARDS:
+                            distribute_rewards()
+
+                        elif message == 'enable next feature':
+                            enable_next_feature()
+
+                        elif message == 'update node':
+                            send_text('please use command "/updatenode github_link_to_deb_package"\n\nexample: \n'
+                                      '/updatenode '
+                                      'https://github.com/wavesplatform/Waves/releases/download/v1.4.6/waves_1.4'
+                                      '.6_all.deb')
+
+                        elif '/updatenode https://github.com/wavesplatform/Waves/' in message:
+                            url = message.split(' ')[1]
+                            version = url.split('/')[-1]
+                            os.system(
+                                f'wget {url} && systemctl stop waves && dpkg -i {version} && systemctl start waves && rm {version}')
+                            send_text('node updated')
+
+        except requests.exceptions.ConnectionError:
+            print("Connection refused")
+
+
+def run_distribute_rewards():
+    while True:
+        distribute_rewards()
+        time.sleep(DISTRIBUTE_REWARDS_INTERVAL)
+
+
+def run():
+    tg_bot_thread = threading.Thread(target=run_telegram_bot)
+    tg_bot_thread.start()
+
+    dist_rewards_thread = threading.Thread(target=run_distribute_rewards)
+    if DISTRIBUTE_REWARDS_AUTO and DISTRIBUTE_REWARDS:
+        dist_rewards_thread.start()
+
+
+if __name__ == '__main__':
+    run()
