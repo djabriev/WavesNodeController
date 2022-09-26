b49a1dd81ff238c99e48494ea25e4af166a7a499
entrypoint <admin@test.com>
2022-09-27 00:51:49 +0500

    Rewrited to pyTelegramBotAPI

--- main.py
+++ main.py
@@ -1,80 +1,32 @@
-# made by @entrypoint
-# works on waves service (docker not supported), tested on ubuntu only
-# node update through deb package
-# your node .conf file should contain features settings (waves.features)
-#   waves {
-#       ...
-#       features {
-#           supported = [17]
-#       }
-#       ...
-#   }
-# script looks for string 'supported = [' then replaces it with 'supported = [id,' // hardcoded like this to avoid
-#                                                                                     external package imports
-
 import json
 import os
-import threading
-
+import telebot
 import requests
-import time
+from telebot import types
 
 TOKEN = 'telegram_bot_tokem'
 URL = 'https://api.telegram.org/bot'
 ADMIN_CHAT_ID = 0  # get your chat id here: @chat_id_echo_bot
 MY_NODE_ADDRESS = 'http://localhost:6870'
 WAVES_NODE_ADDRESS = 'https://nodes.wavesnodes.com'
-DISTRIBUTE_REWARDS = True  # Distribute rewards functionality
-DISTRIBUTE_REWARDS_SCRIPT = '/home/distribute.py'  # path to your distribute rewards python script
 NODE_CONFIG_DIRECTORY = '/usr/share/waves/conf'
 beneficiaryAddress = ''  # your beneficiaryAddress
 
-
-def get_updates(offset=0):
-    result = requests.get(f'{URL}{TOKEN}/getUpdates?offset={offset}').json()
-    return result['result']
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 
-def send_text(bot_message):
-    url = f'{URL}{TOKEN}/sendMessage?chat_id={ADMIN_CHAT_ID}&text={bot_message}'
-    requests.get(url)
+def start(message):
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    btn_1 = types.KeyboardButton('check node height')
+    btn_2 = types.KeyboardButton('check node version')
+    btn_3 = types.KeyboardButton('check node features')
+    btn_4 = types.KeyboardButton('restart node')
+    btn_5 = types.KeyboardButton('update node')
+    btn_6 = types.KeyboardButton('enable next feature')
+    btn_7 = types.KeyboardButton('check beneficiary address balance')
+    markup.add(btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7)
 
-
-def start():
-    kb = json.dumps(
-        {"keyboard":
-            [
-                [
-                    {"text": "check node height"},
-                    {"text": "check node version"},
-                ],
-                [
-                    {"text": "check node features"},
-                    {"text": "check node balance"},
-                ],
-                [
-                    {"text": "restart node"},
-                    {"text": "distribute rewards"},
-                ],
-                [
-                    {"text": "update node"},
-                    {"text": "enable next feature"},
-                ],
-                [
-                    {"text": "check beneficiary address balance"},
-                ],
-            ]
-        }
-    )
-
-    # Create data dict
-    data = {
-        'text': (None, 'Hi!'),
-        'chat_id': (None, ADMIN_CHAT_ID),
-        'reply_markup': (None, kb)
-    }
-
-    requests.post(url=f'{URL}{TOKEN}/sendMessage', headers={}, files=data)
+    bot.send_message(message.from_user.id, 'Hi!', reply_markup=markup)
 
 
 def check_node_height():
@@ -92,35 +44,16 @@ def check_node_features():
     return json.dumps(features, indent=2)
 
 
-def check_node_balance():
-    main_address = requests.get(f'{MY_NODE_ADDRESS}/addresses').json()[0]
-    balance = requests.get(f'{MY_NODE_ADDRESS}/addresses/balance/{main_address}').json()['balance'] / 100000000
-    return f'Node balance: {balance} waves'
-
-
 def check_ben_balance():
     balance = requests.get(f'{MY_NODE_ADDRESS}/addresses/balance/{beneficiaryAddress}').json()['balance'] / 100000000
     return f'Beneficiary address balance: {balance} waves'
 
 
-def distribute_rewards():
-    if DISTRIBUTE_REWARDS:
-        exec(open(DISTRIBUTE_REWARDS_SCRIPT).read())
-        send_text('rewards distributed')
-    else:
-        send_text('functionality disabled')
-
-
-def restart_node():
-    os.system('systemctl restart waves.service')
-    send_text('node restarted')
-
-
-def enable_next_feature():
+def enable_next_feature(message):
     node_features = json.loads(check_node_features())
 
     if node_features[-1]['nodeStatus'] == 'VOTED':
-        send_text('Already enabled')
+        bot.send_message(message.from_user.id, 'Already enabled')
     else:
         with open(f'{NODE_CONFIG_DIRECTORY}/waves.conf') as file:
             file_str = file.read()
@@ -130,75 +63,57 @@ def enable_next_feature():
             with open(f'{NODE_CONFIG_DIRECTORY}/waves.conf', 'w') as outfile:
                 outfile.write(file_str)
 
-            send_text(f'Feature {feature_id} successfully enabled')
+            bot.send_message(message.from_user.id, f'Feature {feature_id} successfully enabled')
 
-            restart_node()
+            restart_node(message)
 
 
-def run_telegram_bot():
-    update_id = 0
-    while True:
-        time.sleep(5)
+def restart_node(message):
+    os.system('systemctl restart waves.service')
+    bot.send_message(message.from_user.id, 'node restarted')
 
-        try:
-            messages = get_updates(update_id)
-            for message in messages:
-                if update_id < message['update_id']:
-                    update_id = message['update_id']
-                    message_chat_id = message['message']['chat']['id']
-                    message = message['message']['text']
 
-                    if ADMIN_CHAT_ID == message_chat_id:
-                        if message == '/start':
-                            start()
+@bot.message_handler(content_types=['text'], chat_types=['private'])
+def get_text_messages(message):
+    if message.from_user.id != ADMIN_CHAT_ID:
+        return
 
-                        elif message == 'check node height':
-                            send_text(check_node_height())
+    if message.text == '/start':
+        start(message)
 
-                        elif message == 'check node version':
-                            send_text(check_node_version())
+    elif message.text == 'check node height':
+        bot.send_message(message.from_user.id, check_node_height())
 
-                        elif message == 'check node features':
-                            send_text(check_node_features())
+    elif message.text == 'check node version':
+        bot.send_message(message.from_user.id, check_node_version())
 
-                        elif message == 'check node balance':
-                            send_text(check_node_balance())
+    elif message.text == 'check node features':
+        bot.send_message(message.from_user.id, check_node_features())
 
-                        elif message == 'check beneficiary address balance':
-                            send_text(check_ben_balance())
+    elif message.text == 'check beneficiary address balance':
+        bot.send_message(message.from_user.id, check_ben_balance())
 
-                        elif message == 'restart node':
-                            restart_node()
+    elif message.text == 'restart node':
+        restart_node(message)
 
-                        elif message == 'distribute rewards':
-                            if DISTRIBUTE_REWARDS:
-                                distribute_rewards()
-                            else:
-                                send_text('DISTRIBUTE_REWARDS disabled')
+    elif message.text == 'enable next feature':
+        enable_next_feature(message)
 
-                        elif message == 'enable next feature':
-                            enable_next_feature()
+    elif message.text == 'update node':
+        bot.send_message(message.from_user.id,
+                         'please use command "/updatenode github_link_to_deb_package"\n\nexample: \n/updatenode '
+                         'https://github.com/wavesplatform/Waves/releases/download/v1.4.6/waves_1.4.6_all.deb')
 
-                        elif message == 'update node':
-                            send_text('please use command "/updatenode github_link_to_deb_package"\n\nexample: \n'
-                                      '/updatenode '
-                                      'https://github.com/wavesplatform/Waves/releases/download/v1.4.6/waves_1.4'
-                                      '.6_all.deb')
-
-                        elif '/updatenode https://github.com/wavesplatform/Waves/' in message:
-                            url = message.split(' ')[1]
-                            version = url.split('/')[-1]
-                            os.system(
-                                f'wget {url} && systemctl stop waves && dpkg -i {version} && systemctl start waves && rm {version}')
-                            send_text('node updated')
-
-        except requests.exceptions.ConnectionError:
-            print("Connection refused to telegram endpoint")
+    elif '/updatenode https://github.com/wavesplatform/Waves/' in message.text:
+        url = message.text.split(' ')[1]
+        version = url.split('/')[-1]
+        os.system(
+            f'wget {url} && systemctl stop waves && dpkg -i {version} && systemctl start waves && rm {version}')
+        bot.send_message(message.from_user.id, 'node updated')
 
 
 def run():
-    tg_bot_thread = threading.Thread(target=run_telegram_bot)
-    tg_bot_thread.start()
+    bot.infinity_polling()
 
 
 if __name__ == '__main__':
